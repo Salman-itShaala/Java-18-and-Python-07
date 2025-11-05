ef931a9ea049916237d68acae59dcbb85c6eb030
salman-itShaala <salman.shaikh@itshaala.com>
2025-11-05 10:10:00 +0530

    added code of todo

--- day 30/script.js
+++ day 30/script.js
@@ -4,20 +4,43 @@ const todoInput = document.getElementById("todo-input");
 const todosWrapper = document.getElementById("todos-wrapper");
 
 function addTodo() {
-    const todoValue = todoInput.value;
-    todos.push(todoValue);
+  const todoValue = todoInput.value;
+  todos.push(todoValue);
 
-    // depending upon that todos array update your html
+  updateDOM();
 
-    // iterating over todos array
-    let htmlToAdd = "";
-    for (let i = 0; i < todos.length; i++) {
-        htmlToAdd = htmlToAdd + `<div class="todo">
-        <p>${todos[i]}</p>
-        <button class="mark-as-done-btn">Mark as done</button>
-        <button class="delete-btn">Delete</button>
-      </div>`;
-    }
-
-    todosWrapper.innerHTML = htmlToAdd;
 }
+
+function deleteTodo(index) {
+  todos.splice(index, 1);
+
+  updateDOM();
+}
+
+
+
+function updateDOM() {
+  let htmlToAdd = "";
+  for (let i = 0; i < todos.length; i++) {
+
+    htmlToAdd = htmlToAdd + `<div class="todo">
+        <p>${todos[i]}</p>
+        <button class="delete-btn" onclick="deleteTodo(${i})">Delete</button>
+      </div>`;
+
+  }
+
+  todosWrapper.innerHTML = htmlToAdd;
+}
+
+
+
+
+
+
+
+
+
+
+
+
